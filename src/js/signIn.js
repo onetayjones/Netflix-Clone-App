f437c1f3be4a1f8690227670c750cffc4e6936ea
onetayjones <taylordjone@gmail.com>
2022-05-11 22:38:07 +0200

    Adding local storage / class & constructor in JS

--- src/js/signIn.js
+++ src/js/signIn.js
@@ -3,3 +3,18 @@
 let form = document.querySelector(".form")
 let usernameInput = document.querySelector(".username")
 let passwordInput = document.querySelector(".password")
+
+
+// local storage key & user array
+
+const STORAGE_KEY = "sign-in-user-storage-key"
+let userArray = [];
+
+// User class & constructor
+
+class USER {
+  constructor(username,password){
+    this.username = username;
+    this.password = password;
+  }
+}
