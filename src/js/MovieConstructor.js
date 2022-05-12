1395334ee0c8476b5a67a2c7ec79dd639ab08f96
onetayjones <taylordjone@gmail.com>
2022-05-12 20:00:54 +0200

    Setting up homepage HTML & Movie Constructor

---
+++ src/js/MovieConstructor.js
@@ -0,0 +1,14 @@
+// Class Constructor
+
+export class MovieList {
+
+  constructor(id, name, genre, duration, description, releaseDate, comingSoon, thumbnail )
+  this.id = id;
+  this.name = name;
+  this.genre = genre;
+  this.duration = duration;
+  this.description = description;
+  this.releaseDate = releaseDate;
+  this.comingSoon = comingSoon;
+  this.thumbnail = thumbnail; 
+}
