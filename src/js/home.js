181550ee610a9bc074029dd808f12c8ff709dd5d
onetayjones <taylordjone@gmail.com>
2022-05-14 10:01:28 +0200

    creating dropdown menu over avatar

---
+++ src/js/home.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+// User clicks on Avatar button
+function myButton(){
+  getElementById('myDropdown').classList.toggle('show');
+}
+
+//Close the myDropdown
+window.onclick = function(event){
+
+}
